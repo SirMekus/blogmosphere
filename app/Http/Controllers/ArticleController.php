80c945381743e5d51b571f389afcdad48ba791af
webloit-dev <webloit2020@gmail.com>
2023-01-31 12:53:23 +0100

    Documentation still in progress

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,7 +13,6 @@ class ArticleController extends Controller
      *     @OA\Response(response="200", description="Returns all the articles")
      * )
      */
-
     public function articles()
     {
         $articles = Article::orderBy('created_at', 'desc')->paginate(10);
@@ -48,6 +47,23 @@ class ArticleController extends Controller
         return request()->wantsJson() ? $article : view('blog', ['article' => $article]);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/articles/{id}/view",
+     *     description="Increments number of views for a particular article",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of article to increment its view",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Returns an integer which represents the updated number of views for the article")
+     * )
+     */
     public function recordView($id)
     {
         $article = Article::findOrFail($id);
