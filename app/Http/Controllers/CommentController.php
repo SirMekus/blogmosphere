7625c8f2e4f762483f81cc3269b30cc2a7c2a6eb
webloit-dev <webloit2020@gmail.com>
2023-01-31 13:16:48 +0100

    Documentation done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,10 +6,68 @@ use App\Models\Comment;
 
 class CommentController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/articles/{id}/comment",
+     *     description="Creates comment on a particular article",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of article to comment on",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Invalid input or incomplete form entry"
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful"
+     *     ),
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="name",
+     *                     description="name of person making the comment",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     description="Email address of the person making the comment.",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="subject",
+     *                     description="subject of the comment",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="article_id",
+     *                     description="ID of article for the comment",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="body",
+     *                     description="Actual comment itself",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function commentOnArticle($id)
     {
         $message = [
-            'body.required'=>"Please write a comment"
+            'body.required' => "Please write a comment"
         ];
 
         request()->validate([
@@ -18,13 +76,13 @@ class CommentController extends Controller
             'subject' => ['bail', 'required', 'string'],
             'article_id' => ['bail', 'string', 'exists:articles,id'],
             'body' => ['bail', 'required', 'string'],
-            ], $message);
+        ], $message);
 
-        Comment::updateOrCreate(
-                ['email' => request()->email],
-                ['name' => request()->name, 'subject' => request()->subject
-                , 'article_id' => request()->article_id, 'body' => request()->body]
-            );
+        Comment::create(
+            [
+                'name' => request()->name, 'email' => request()->email, 'subject' => request()->subject, 'article_id' => request()->article_id, 'body' => request()->body
+            ]
+        );
 
         return response("Your comment was sent successfully");
     }
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
 use App\Models\Like;
 
 class LikeController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/articles/{id}/like",
+     *     description="Endpoint to like a particular article",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of article to like",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Returns an integer which represents the updated number of likes for the article")
+     * )
+     */
     public function likeArticle($id)
     {
         $article = Article::withCount('likes')->findOrFail($id);
